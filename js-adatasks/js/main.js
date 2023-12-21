017536d7c9f9756fa89c66341fc159d849384c39
Marta <marta.af.lab@gmail.com>
2023-12-21 21:35:41 +0100

    add clic

--- js-adatasks/js/main.js
+++ js-adatasks/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 //QUERYSELECTOR
 const taskList = document.querySelector('.js_task-list');
-
+// const
 //VARIABLES
-
+const inputCheck = `<input type="checkbox" class="js_ckeck"/>`;
 const tasks = [
     { name: "Recoger setas en el campo", completed: true },
     { name: "Comprar pilas", completed: true },
@@ -16,8 +16,23 @@ const tasks = [
 
 for (const task of tasks) {
     if (task.completed === true) {
-        taskList.innerHTML += `<li class="tachado">${task.name}</li>`;
+        taskList.innerHTML += 
+        `<li class="tachado">${task.name} 
+        <input type="checkbox" checked/>
+        </li>`;
+
     } else {
-        taskList.innerHTML += `<li>${task.name}</li>`;
+        taskList.innerHTML += `<li>${task.name} ${inputCheck}</li>`;
     }
 }
+
+// function clickinput () {
+
+// }
+// clickinput()
+console.log(taskList);
+console.log(inputCheck);
+
+inputCheck.addEventListener('click', (event) => {
+`<li class="tachado">${task.name}`;
+});
